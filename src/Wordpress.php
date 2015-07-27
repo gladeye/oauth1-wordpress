bf5871ea8c658b5ef55349f7c2039248ba55ea22
Ken Vu <ken@gladeye.co.nz>
2015-07-27 16:58:08 +1200

    Add initial files

---
+++ src/Wordpress.php
@@ -0,0 +1,11 @@
+<?php
+namespace Gladeye\OAuth1\Client\Server;
+
+use League\OAuth1\Client\Credentials\TokenCredentials;
+use League\OAuth1\Client\Server\Server;
+use League\OAuth1\Client\Server\User;
+
+class Wordpress extends Server
+{
+
+}
---
+++ tests/src/WordpressTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace Gladeye\OAuth1\Client\Test\Server;
+
+use Gladeye\OAuth1\Client\Server\Wordpress;
+use Mockery as m;
+
+class WordpressTest extends \PHPUnit_Framework_TestCase
+{
+
+}
